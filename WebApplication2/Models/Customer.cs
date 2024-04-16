b85a87de4ba49a40a9d1b9806ba28ed4fbd2427b
firas <firasshdad@gmail.com>
2024-04-16 11:49:30 +0300

    Add project files.

---
+++ WebApplication2/Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication2.Models
+{
+    public class Customer
+    {
+        public string CivilId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+
+    }
+}
