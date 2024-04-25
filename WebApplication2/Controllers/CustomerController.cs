7b88a532a42a9440976c881524e82a51ac937adf
firas <firasshdad@gmail.com>
2024-04-25 09:36:03 +0300

    ReUploading

--- WebApplication2/Controllers/CustomerController.cs
+++ WebApplication2/Controllers/CustomerController.cs
@@ -5,15 +5,28 @@ namespace WebApplication2.Controllers
 {
     public class CustomerController : Controller
     {
-        public IActionResult Index()
-        {
-            var customers = new List<Customer>();
-            customers.Add(new Customer(1234567, "Feras Alshadad", "f@gmail.com", 34567898));
-            customers.Add(new Customer(0987602, "Fahad Ahmed", "f1@gmail.com", 45678761));
-            customers.Add(new Customer(0098763, "Fawaz Bader", "f2@gmail.com", 34567654));
-            customers.Add(new Customer(0345304, "Faisal Talal", "f@3gmail.com", 23456762));
+        
+            static List<Customer> Customers = new List<Customer>();
+            public IActionResult Index()
+            {
+                return View(Customers);
+            }
 
-            return View(customers);
-        }
+            public IActionResult AddCustomer()
+            {
+                return View();
+            }
+
+            [HttpPost]
+            public IActionResult AddCustomer(Customer customer)
+            {
+                if (ModelState.IsValid)
+                {
+                    Customers.Add(customer);
+                    return RedirectToAction("Index");
+                }
+                return View(customer);
+            }
     }
-}
+ }
+
--- WebApplication2/Models/Customer.cs
+++ WebApplication2/Models/Customer.cs
@@ -1,21 +1,21 @@
-﻿namespace WebApplication2.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Models
 {
     public class Customer
     {
+        [RegularExpression(@"^1\d{7}$",
+          ErrorMessage = "An eight digit long number starting with 1 required")]
         public int CivilId { get; set; }
+        [Required]
+        [StringLength(20, MinimumLength = 3)]
         public string Name { get; set; }
+        [EmailAddress]
         public string Email { get; set; }
-
+        [Phone]
         public int Phone { get; set; }
 
-        public Customer(int CivilId, string Name, string Email, int Phone)
-        {
-            this.CivilId = CivilId;
-            this.Name = Name;
-            this.Email = Email;
-            this.Phone = Phone;
 
-        }
 
     }
 }
